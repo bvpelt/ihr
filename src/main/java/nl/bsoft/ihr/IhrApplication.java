3f1c8f45656afc21c2b01d980c1db18ca2d92fe9
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2024-02-16 19:10:11 +0100

    First commit

---
+++ src/main/java/nl/bsoft/ihr/IhrApplication.java
@@ -0,0 +1,13 @@
+package nl.bsoft.ihr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhrApplication.class, args);
+	}
+
+}
